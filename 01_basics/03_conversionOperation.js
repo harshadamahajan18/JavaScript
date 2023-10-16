9d563d26f580c9211666ce8496911bafc8b75823
Harshada Mahajan <148041508+harshadamahajan18@users.noreply.github.com>
2023-10-16 09:37:37 +0000

    Data Type Conversion Done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,77 @@
+let score = "harshada"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// NOTES:-
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// NOTES:-
+// 1 => true; 0 => false
+// "" => false
+//  "harshada" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************** Operations ************************
+
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(282);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " harshada"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// Do not use this
+// console.log( (3 + 4) * 5 % 3);
+
+// Do not use this
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// Read prefix and postfix from mdn 
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
