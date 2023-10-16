266574f27f5e172c40a5b8ce7b3590d8b9f570ba
Harshada Mahajan <148041508+harshadamahajan18@users.noreply.github.com>
2023-10-16 08:05:46 +0000

    DataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser so this will not work here in nodejs
+
+console.log(3 
+    + 
+    3)  // code readability should be high do not write code like this
+
+console.log("Harshada")
+
+let name = "harshada"
+let age = 18
+let isLoggedIn = false
+// let state; // undefined
+let state = null // gives null
+
+//  number => 2 to power 53
+//  bigint 
+//  string => ""
+//  boolean => true/false
+//  null => standalone value(empty value representation)
+//  undefined => 
+//  symbol => unique
+
+
+//  object
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
